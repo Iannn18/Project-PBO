aae870014858f6e9b09bf309392bedd9db1f1548
Phisict <adrianfathan69@gmail.com>
2024-05-12 22:09:04 +0700

    tedst

--- Project-akhir-PBO/obj/Debug/net8.0-windows/Project-akhir-PBO.AssemblyInfo.cs
+++ Project-akhir-PBO/obj/Debug/net8.0-windows/Project-akhir-PBO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b563d37f971b0def8c6a1e561cb55aa87dc1009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39aa38e06467d4e749b9a4978c3f779e1382f867")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
