fda8a31bace0206fc5f8c73a6f31b7cc220f032e
Phisict <adrianfathan69@gmail.com>
2024-05-12 22:15:04 +0700

    fix bug

--- Project-akhir-PBO/obj/Debug/net8.0-windows/Project-akhir-PBO.AssemblyInfo.cs
+++ Project-akhir-PBO/obj/Debug/net8.0-windows/Project-akhir-PBO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae870014858f6e9b09bf309392bedd9db1f1548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682e99c06a362c9207d1e2bddb1dac6481dfe751")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
