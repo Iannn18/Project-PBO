443dc135103a921de180f93a4a752095fc3766f0
Iannnn <232410103085@mail.unej.ac.id>
2024-05-15 23:46:28 +0700

    update and create some form

--- Project-akhir-PBO/obj/Debug/net8.0-windows/Project-akhir-PBO.AssemblyInfo.cs
+++ Project-akhir-PBO/obj/Debug/net8.0-windows/Project-akhir-PBO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea232d693c789bf08c8be91a123ff5b2d613c69d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f9e2aa7c8b2a4771e215ac287355f5668cf9e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
