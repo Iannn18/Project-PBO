768bf7d37d59fa55107d75d60459a252b8f69db4
akbar2611 <232410103034@mail.unej.ac.id>
2024-05-15 10:36:26 +0700

    akbar

--- Project-akhir-PBO/obj/Debug/net8.0-windows/Project-akhir-PBO.AssemblyInfo.cs
+++ Project-akhir-PBO/obj/Debug/net8.0-windows/Project-akhir-PBO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f316a86ccd0c85215605a9fcd577a25cb4bdfe93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f71bfcfea0ba777ad5a4aabe6fe3adcb54f22a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-akhir-PBO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
