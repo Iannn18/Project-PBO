f027bd38b05c1df26536290b2ea9ea34060b0106
Phisict <adrianfathan69@gmail.com>
2024-05-17 00:56:35 +0700

    Add Kelas,Jabatan,Person,Siswa,Staff Model

---
+++ Project-akhir-PBO/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_akhir_PBO.Model
+{
+	internal class Person
+	{
+		[Required]
+		public DateOnly Tanggal_Lahir { get; set; }
+		public string Tempat_Lahir { get; set; }
+		public string Alamat {  get; set; }
+	}
+}
---
+++ Project-akhir-PBO/Model/Siswa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_akhir_PBO.Model
+{
+	internal class Siswa : Person
+	{
+		[Key]
+		public int NISN { get; set; }
+		[Required]
+		public string Nama_Siswa{ get; set; }
+		[ForeignKey("Id_Kelas")]
+		public virtual Kelas Kelas { get; set; }
+
+	}
+}
