2604a4755b6eecb79a7cd17333c6b361f6d8fc83
Phisict <adrianfathan69@gmail.com>
2024-06-08 22:38:01 +0700

    Mapel Tugas List and Click Handler on each child

--- Project-akhir-PBO/view/Mapeltugas.Designer.cs
+++ Project-akhir-PBO/view/Mapeltugas.Designer.cs
@@ -33,15 +33,10 @@
             labelnamaMapel = new Label();
             labelKelas = new Label();
             dataGridView1 = new DataGridView();
-            panel1 = new Panel();
-            flowLayoutPanel1 = new FlowLayoutPanel();
-            buttonDetailtgs1 = new Button();
-            buttonTambahTugas = new Button();
             Column1 = new DataGridViewTextBoxColumn();
             Column2 = new DataGridViewComboBoxColumn();
+            flowLayoutPanel1 = new FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            panel1.SuspendLayout();
-            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // labelnamaMapel
@@ -74,53 +69,9 @@
             dataGridView1.Location = new Point(695, 82);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(418, 561);
+            dataGridView1.Size = new Size(418, 528);
             dataGridView1.TabIndex = 2;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-            // 
-            // panel1
-            // 
-            panel1.AutoScroll = true;
-            panel1.BackColor = SystemColors.ControlLight;
-            panel1.Controls.Add(flowLayoutPanel1);
-            panel1.Location = new Point(412, 38);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(203, 543);
-            panel1.TabIndex = 3;
-            panel1.Paint += panel1_Paint;
-            // 
-            // flowLayoutPanel1
-            // 
-            flowLayoutPanel1.AutoScroll = true;
-            flowLayoutPanel1.Controls.Add(buttonDetailtgs1);
-            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
-            flowLayoutPanel1.Location = new Point(3, 12);
-            flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(194, 528);
-            flowLayoutPanel1.TabIndex = 1;
-            // 
-            // buttonDetailtgs1
-            // 
-            buttonDetailtgs1.ForeColor = Color.Black;
-            buttonDetailtgs1.Location = new Point(3, 3);
-            buttonDetailtgs1.Name = "buttonDetailtgs1";
-            buttonDetailtgs1.Size = new Size(182, 94);
-            buttonDetailtgs1.TabIndex = 0;
-            buttonDetailtgs1.Text = "Tugas 1";
-            buttonDetailtgs1.UseVisualStyleBackColor = true;
-            // 
-            // buttonTambahTugas
-            // 
-            buttonTambahTugas.AutoSize = true;
-            buttonTambahTugas.Font = new Font("Segoe UI", 25F);
-            buttonTambahTugas.ForeColor = Color.Black;
-            buttonTambahTugas.Location = new Point(476, 599);
-            buttonTambahTugas.Name = "buttonTambahTugas";
-            buttonTambahTugas.Size = new Size(92, 56);
-            buttonTambahTugas.TabIndex = 2;
-            buttonTambahTugas.Text = "+";
-            buttonTambahTugas.UseVisualStyleBackColor = true;
-            buttonTambahTugas.Click += buttonTambahTugas_Click;
             // 
             // Column1
             // 
@@ -142,13 +93,22 @@
             Column2.Resizable = DataGridViewTriState.True;
             Column2.Width = 125;
             // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.AutoScrollMargin = new Size(0, 100);
+            flowLayoutPanel1.Location = new Point(410, 82);
+            flowLayoutPanel1.Margin = new Padding(0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(279, 509);
+            flowLayoutPanel1.TabIndex = 4;
+            // 
             // Mapeltugas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1213, 591);
-            Controls.Add(buttonTambahTugas);
-            Controls.Add(panel1);
+            Controls.Add(flowLayoutPanel1);
             Controls.Add(dataGridView1);
             Controls.Add(labelKelas);
             Controls.Add(labelnamaMapel);
@@ -157,8 +117,6 @@
             Text = "Mapeltugas";
             Load += Mapeltugas_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            panel1.ResumeLayout(false);
-            flowLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -168,12 +126,9 @@
         private Label labelnamaMapel;
         private Label labelKelas;
         private DataGridView dataGridView1;
-        private Panel panel1;
         private Button buttonTugas1;
-        private Button buttonTambahTugas;
-        private FlowLayoutPanel flowLayoutPanel1;
-        private Button buttonDetailtgs1;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewComboBoxColumn Column2;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- Project-akhir-PBO/view/Mapeltugas.cs
+++ Project-akhir-PBO/view/Mapeltugas.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Project_akhir_PBO.Context;
+using Project_akhir_PBO.view;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,9 +29,72 @@ namespace Project_akhir_PBO
         {
 
         }
+        // Define the event handler outside of Mapeltugas_Load
+        private void ListTugasControll_Click(object sender, EventArgs e)
+        {
+            // Get the parent control of the sender and cast it to ListTugasControll
+            ListTugasControll clickedControl = ((Control)sender).Parent as ListTugasControll;
+
+            if (clickedControl != null)
+            {
+                // Show a MessageBox
+                MessageBox.Show($"You clicked {clickedControl.judul}" +
+                    $"\n{clickedControl.deskripsi}" +
+                    $"\n{clickedControl.id}" +
+                    $"\n{clickedControl.id_mapel}" +
+                    $"\n{clickedControl.id_status}");
+            }
+        }
 
         private void Mapeltugas_Load(object sender, EventArgs e)
         {
+
+            ListTugasControll[] listTugasControlls = new ListTugasControll[10];
+            void populateFlow()
+            {
+                PenugasanContext penugasanContext = new PenugasanContext();
+                DataTable penugasan = PenugasanContext.all();
+                foreach (DataRow row in penugasan.Rows)
+                {
+                    ListTugasControll control = new ListTugasControll();
+                    control.judul = row["nama_tugas"].ToString();
+                    control.deskripsi = row["deskripsi_tugas"].ToString();
+                    control.id = row["id_tugas"].ToString();
+                    control.id_mapel = row["kode_mapel"].ToString();
+                    control.id_status = row["id_status"].ToString();
+                    control.Name = row["id_tugas"].ToString();
+                    control.Click += ListTugasControll_Click;
+                    foreach (Control childControl in control.Controls)
+                    {
+                        childControl.Click += ListTugasControll_Click;
+                    }
+
+                    flowLayoutPanel1.Controls.Add(control);
+                }
+                /*                for (int i = 0; i < 10; i++)
+                                {
+                                    listTugasControlls[i] = new ListTugasControll();
+                                    listTugasControlls[i].judul = "Tugas " + i;
+                                    listTugasControlls[i].deskripsi = "Deskripsi Tugas " + i;
+                                    listTugasControlls[i].Name = "Tugas " + i;
+                                    listTugasControlls[i].Click += ListTugasControll_Click;
+                                    foreach (Control childControl in listTugasControlls[i].Controls)
+                                    {
+                                        childControl.Click += ListTugasControll_Click;
+                                    }
+
+                                    if (flowLayoutPanel1.Controls.Count < 0)
+                                    {
+                                        flowLayoutPanel1.Controls.Clear();
+                                    }
+                                    else
+                                    {
+
+                                        flowLayoutPanel1.Controls.Add(listTugasControlls[i]);
+                                    }
+                                }*/
+            }
+            populateFlow();
 
         }
 
